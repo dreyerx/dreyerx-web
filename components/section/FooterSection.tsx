fe1175e75798d7cb83f8e9871668ff399a24728d
billalxcode <billal.xcode@gmail.com>
2024-08-02 21:54:07 +0700

    add dynamic footer

--- components/section/FooterSection.tsx
+++ components/section/FooterSection.tsx
@@ -1,8 +1,56 @@
-import { Box, Flex, Heading, Image, Link, ListItem, Text } from '@chakra-ui/react'
-import { faGithub, faMedium, faTelegram } from '@fortawesome/free-brands-svg-icons'
-import { faX } from '@fortawesome/free-solid-svg-icons'
+import { Box, Flex, Heading, Image, Link, LinkBox, LinkOverlay, ListItem, Text } from '@chakra-ui/react'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import FooterData from "@/data/footer.json"
+import { faGithub, faMedium, faTelegram } from '@fortawesome/free-brands-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
+const iconDefinition = {
+  "telegram": faTelegram,
+  "github": faGithub,
+  "medium": faMedium,
+  "x": faX
+} as const
+
+type IconKeys = keyof typeof iconDefinition;
+
+function FooterLink(props: { name: string, url: string }) {
+  return <Link key={props.name} href={props.url}>{props.name}</Link>
+}
+
+function FooterLinkIcon(props: { icon: IconKeys, url: string }) {
+  return (
+    <LinkBox key={props.icon}>
+      <LinkOverlay href={props.url}>
+        <FontAwesomeIcon size='xl' icon={iconDefinition[props.icon]} />
+      </LinkOverlay>
+    </LinkBox>
+  )
+}
+
+function FooterLinkRender(props: { data: Array<{ title: string, links: Array<{ name?: string, url: string, icon?: string }> }> }) {
+  console.log(props.data)
+  return props.data.map((footer) => {
+    return (
+      <Flex gap={3} flexDirection={'column'} key={footer.title}>
+        <Heading size={'sm'} mb={3} textTransform={'uppercase'}>
+          {footer.title}
+        </Heading>
+
+        {
+          footer.links.map((link) => {
+            if (typeof link.icon === "undefined" && typeof link.name === "string") {
+              return FooterLink({ name: link.name, url: link.url })
+            } else if (typeof link.icon == "string") {
+              return FooterLinkIcon({ icon: link.icon as IconKeys, url: link.url })
+            }
+          })
+        }
+      </Flex>
+    )
+  })
+}
 
 export default function FooterSection() {
   return (
@@ -10,67 +58,9 @@ export default function FooterSection() {
       <Box mx={[0, 20]}>
         <Flex align={'start'} justify={'space-between'} flexDirection={['column', 'row']} gap={[10, 0]}>
           <Image src='assets/images/dreyerx-typograph.png' width={'200px'} alt='DreyerX Logo' />
-          <Flex gap={3} flexDirection={'column'}>
-            <Heading size={'sm'} mb={3} textTransform={'uppercase'}>
-              Explore
-            </Heading>
-            <Link>
-              Getting Started
-            </Link>
-            <Link>
-              Staking
-            </Link>
-            <Link>
-              Bridge
-            </Link>
-            <Link>
-              Swap (Comming Soon)
-            </Link>
-          </Flex>
-          <Flex gap={3} flexDirection={'column'}>
-            <Heading size={'sm'} mb={3} textTransform={'uppercase'}>
-              EVM
-            </Heading>
-            <Link>
-              Testnet
-            </Link>
-            <Link>
-              Mainnet
-            </Link>
-          </Flex>
-          <Flex gap={3} flexDirection={'column'}>
-            <Heading size={'sm'} mb={3} textTransform={'uppercase'}>
-              Build with DreyerX
-            </Heading>
-            <Link>
-              Docs
-            </Link>
-            <Link>
-              SDK (Comming Soon)
-            </Link>
-            <Link>
-              Academy (Comming Soon)
-            </Link>
-          </Flex>
-          <Flex gap={3} flexDirection={'column'}>
-            <Heading size={'sm'} mb={3} textTransform={'uppercase'}>
-              Community
-            </Heading>
-            <Flex gap={3}>
-              <Link>
-                <FontAwesomeIcon size='xl' icon={faTelegram} />
-              </Link>
-              <Link>
-                <FontAwesomeIcon size='xl' icon={faGithub} />
-              </Link>
-              <Link>
-                <FontAwesomeIcon size='xl' icon={faMedium} />
-              </Link>
-              <Link>
-                <FontAwesomeIcon size='xl' icon={faX} />
-              </Link>
-            </Flex>
-          </Flex>
+          {
+            FooterLinkRender({ data: FooterData.data })
+          }
         </Flex>
         <Text textAlign={'end'} mt={'60px'}>Copyright &copy; 2024 DreyerX</Text>
       </Box>
