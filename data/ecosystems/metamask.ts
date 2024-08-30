d2295bb965b0b1c84529a240e6b1ca4fdd602de3
billalxcode <billal.xcode@gmail.com>
2024-08-30 19:49:56 +0700

    add metamask ecosystem

--- data/ecosystems/metamask.ts
+++ data/ecosystems/metamask.ts
@@ -3,7 +3,7 @@ import defineEcosystem from "@/libs/defineEcosystem";
 export const metamask = defineEcosystem({
     name: 'MetaMask',
     category: 'wallet',
-    image: 'https://metamask.io/images/metamask.png',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/512px-MetaMask_Fox.svg.png',
     url: 'https://metamask.io',
     description: 'MetaMask is a crypto wallet and gateway to blockchain apps, enabling users to manage their Ethereum wallets.'
 });
