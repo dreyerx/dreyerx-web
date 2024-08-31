55f2cf31626abfea9503a1a9b51e089642e042d1
billalxcode <billal.xcode@gmail.com>
2024-08-31 13:02:45 +0700

    add version component

--- components/section/FooterSection.tsx
+++ components/section/FooterSection.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex, Heading, Image, Link, LinkBox, LinkOverlay, ListItem, Text } from '@chakra-ui/react'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FooterData from "@/data/footer.json"
 import { faGithub, faMedium, faTelegram } from '@fortawesome/free-brands-svg-icons'
 import { faX } from '@fortawesome/free-solid-svg-icons'
@@ -14,6 +14,16 @@ const iconDefinition = {
 } as const
 
 type IconKeys = keyof typeof iconDefinition;
+
+async function getLastestVersion() {
+  const response = await fetch('https://raw.githubusercontent.com/dreyerx/dreyerx-web/main/package.json', {
+    headers: {
+      "Accept": "application/vnd.github+json"
+    }
+  })
+  const response_json = await response.json()
+  return response_json['version']
+} 
 
 function FooterLink(props: { name: string, url: string }) {
   return <Link key={props.name} href={props.url}>{props.name}</Link>
@@ -30,7 +40,6 @@ function FooterLinkIcon(props: { icon: IconKeys, url: string }) {
 }
 
 function FooterLinkRender(props: { data: Array<{ title: string, links: Array<{ name?: string, url: string, icon?: string }> }> }) {
-  console.log(props.data)
   return props.data.map((footer) => {
     return (
       <Flex gap={3} flexDirection={'column'} key={footer.title}>
@@ -53,18 +62,33 @@ function FooterLinkRender(props: { data: Array<{ title: string, links: Array<{ n
 }
 
 export default function FooterSection() {
+  const [gitLatestVersion, setGitLatestVersion ] = useState()
+
+  useEffect(() => {
+    (async() => {
+      const gitLatestVersion_response = await getLastestVersion()
+
+      setGitLatestVersion(gitLatestVersion_response)
+    })()
+  }, [gitLatestVersion])
+
   return (
     <Flex width={'full'} flexDirection={'column'} bgColor={'footer'} p={[10, '60px']} bottom={0} py={20}>
       <Box mx={[0, 20]}>
         <Flex align={'start'} justify={'space-between'} flexDirection={['column', 'row']} gap={[10, 0]}>
-          <Image src='assets/images/dreyerx-typograph.png' width={'200px'} alt='DreyerX Logo' />
+          <Flex flexDirection={['column', 'column']} w={'500px'} gap={5}>
+            <Image src='assets/images/dreyerx-typograph.png' width={'200px'} alt='DreyerX Logo' />
+            <Text>
+              Empowering transactions with speed and security on our POA-powered Layer 1 blockchain project
+            </Text>
+          </Flex>
           <Flex gap={[10, 90]} flexDirection={['column', 'row']}>
             {
               FooterLinkRender({ data: FooterData.data })
             }
           </Flex>
         </Flex>
-        <Text textAlign={'end'} mt={'60px'}>Copyright &copy; 2024 DreyerX</Text>
+        <Text textAlign={'end'} mt={'60px'}>Copyright &copy; 2024 DreyerX - v{ gitLatestVersion }</Text>
       </Box>
     </Flex>
   )
