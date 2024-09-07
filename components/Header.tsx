095d080d2b5ea694d151aa49adb5eb404a3c5ce7
billalxcode <billal.xcode@gmail.com>
2024-09-07 13:21:01 +0700

    remove shadow in header and change it to border

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import { Box, Button, Collapse, Divider, Fade, Flex, Heading, Image, Link, LinkBox, LinkOverlay, Slide, Text } from '@chakra-ui/react'
 import { faBars, faClose } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 export default function Header() {
   const [isActiveBar, setIsActiveBar] = useState(false)
+  const pathname = usePathname()
 
   useEffect(() => {
     if (isActiveBar) {
@@ -16,7 +18,7 @@ export default function Header() {
 
   return (
     <>
-      <Flex bgColor={'bg'} p={[5, 0, 5]} px={[10, 10, 20]} shadow={'md'} width={'full'} borderRadius={10} position={'fixed'} top={0} zIndex={100}>
+      <Flex bgColor={'bg'} p={[5, 0, 5]} px={[10, 10, 20]} borderWidth={1} borderColor={'white40'} width={'full'} borderBottomRadius={10} position={'fixed'} top={0} zIndex={100}>
         <Flex justify={'space-between'} align={'center'} w={'full'} mx={[0, 0, 20]}>
           <LinkBox>
             <LinkOverlay href='/'>
@@ -29,10 +31,18 @@ export default function Header() {
           </Button>
 
           <Flex gap={'40px'} display={['none', 'flex', 'flex']}>
-            <Link href='/' color={'text'}>Home</Link>
-            <Link href='/ecosystem'>Ecosystem</Link>
-            <Link href='https://docs.dreyerx.com/architecrure/tokenomics'>Tokenmonics</Link>
-            <Link href='/#roadmap'>Roadmap</Link>
+            <Link href='/' color={pathname == '/' ? 'text' : 'text95'} transition={'all .3s ease-in-out'} _hover={{
+              color: 'white'
+            }}>Home</Link>
+            <Link href='/ecosystem' color={pathname == '/ecosystem' ? 'text' : 'text95'} transition={'all .3s ease-in-out'} _hover={{
+              color: 'white'
+            }}>Ecosystem</Link>
+            <Link href='https://docs.dreyerx.com/architecrure/tokenomics' color={pathname == '/tokenmonics' ? 'text' : 'text95'} transition={'all .3s ease-in-out'} _hover={{
+              color: 'white'
+            }}>Tokenmonics</Link>
+            <Link href='/#roadmap' color={pathname == '/roadmap' ? 'text' : 'text95'} transition={'all .3s ease-in-out'} _hover={{
+              color: 'white'
+            }}>Roadmap</Link>
           </Flex>
 
           <Link href='https://docs.dreyerx.com' bgColor={'primary'} display={['none', 'none', 'block']} size={'md'} p={2} px={3} borderRadius={5} fontWeight={'bold'} fontSize={15} transition={'.5s ease-in-out'} _hover={{ textDecoration: 'none', bgColor: 'hover.primary' }}>
@@ -44,7 +54,7 @@ export default function Header() {
       <Box w={'full'} h={'full'} display={['block', 'none']} zIndex={1000}>
         <Fade in={isActiveBar}>
           <Flex display={[isActiveBar ? 'block' : 'none', 'none']} h={'100%'} flexDirection={"column"} position={'fixed'} w={'full'} top={20} p={7} bgColor={'bg'} backdropFilter={'blur(5px)'} borderRadius={10} zIndex={99}>
-            <Box bgColor={'primary'} w={'full'} h={100} position={'absolute'} filter={'blur(50px)'} opacity={.5}>
+            <Box bgColor={'primary'} w={'full'} h={'200'} position={'absolute'} filter={'blur(50px)'} opacity={.2}>
 
             </Box>
             <Heading size={'sm'} mt={5}>Use DreyerX</Heading>
